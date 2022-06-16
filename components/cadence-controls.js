b82347fbf287451990cdb5c93333cf2ecf7fd05b
artemshar <artemrandom@ya.ru>
2022-06-17 00:49:50 +0300

    added min/max values

--- components/cadence-controls.js
+++ components/cadence-controls.js
@@ -1,11 +1,17 @@
 import styles from './cadence-controls.module.scss'
 
-export default function CadenceControls({cadence, updateCadence}) {
+export default function CadenceControls({ cadence, updateCadence }) {
+  const maxRangeValue = 200;
+  const minRangeValue = 70;
 
   return (
     <div className={styles.cadenceControls}>
       <div className={styles.cadenceControls__value}>{cadence}</div>
-      <input onChange={(e) => updateCadence(e.target.value)} className={styles.cadenceControls__input} type="range" id="bpm" name="bpm" min="140" max="190"/>
+      <div className={styles.cadenceControls__range}>
+        <span className={styles.cadenceControls__rangeMin}>{minRangeValue}</span>
+        <span className={styles.cadenceControls__rangeMax}>{maxRangeValue}</span>
+        <input onChange={(e) => updateCadence(e.target.value)} className={styles.cadenceControls__rangeInput} type="range" id="bpm" name="bpm" min={minRangeValue} max={maxRangeValue} />
+      </div>
     </div>
   )
 }
